ac58e9fa99fb4b3052c035b015c4f8fc2ffc5261
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2022-03-13 10:11:25 -0500

    add wall jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed = 5.3f;
+    public float Speed = 6f;
     public float JumpForce = 5.3f;
+    public float WallXJumpForce = 6f;
+    public float WallYJumpForce = 15f;
+    public float wallCheckDistance = 0.42f;
     public bool debug;
 
     private float _horizontal;
     private bool _jumping;
     private bool _grounded = false;
+    private bool _leftWall = false;
+    private bool _rightWall = false;
 
     private Rigidbody2D _rigidBody2D;
     private CircleCollider2D _circleCollider2D;
     [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private LayerMask wallLayerMask;
 
-    bool OnGround()
+    /**
+     * Check if player is facing a wall with a certain distance
+     */
+    bool OnWall(float distance)
     {
-        float extraHeight = 0.1f;
-        Bounds colliderBounds = _circleCollider2D.bounds;
-
-        RaycastHit2D raycastHit = Physics2D.BoxCast(colliderBounds.center, colliderBounds.size, 0f, Vector2.down, extraHeight, platformLayerMask);
+        RaycastHit2D raycastHit = Physics2D.Raycast(this.transform.position, Vector2.left, -distance, wallLayerMask);
         bool isHit = raycastHit.collider != null;
 
         if (debug)
@@ -31,22 +37,58 @@ public class PlayerMovement : MonoBehaviour
             {
                 rayColor = Color.green;
             }
-            Debug.DrawRay(colliderBounds.center + new Vector3(colliderBounds.extents.x, 0), Vector2.down * (colliderBounds.extents.y + extraHeight), rayColor);
-            Debug.DrawRay(colliderBounds.center - new Vector3(colliderBounds.extents.x, 0), Vector2.down * (colliderBounds.extents.y + extraHeight), rayColor);
-            Debug.DrawRay(colliderBounds.center - new Vector3(colliderBounds.extents.x, colliderBounds.extents.y + extraHeight), Vector2.right * ( 2 * colliderBounds.extents.x), rayColor);
+            Debug.DrawRay(this.transform.position, new Vector2(distance, 0), rayColor);
         }
 
         return isHit;
     }
 
+    /** 
+     * Determines if the player is touching a platform     
+     */
+    bool OnGround()
+    {
+        float extraHeight = 0.1f;
+        float adjustOnGroundWidth = 0.1f;
+        Bounds colliderBounds = _circleCollider2D.bounds;
+
+        RaycastHit2D raycastHit = Physics2D.BoxCast(colliderBounds.center, new Vector3(colliderBounds.size.x - adjustOnGroundWidth, colliderBounds.size.y, colliderBounds.size.z), 0f, Vector2.down, extraHeight, platformLayerMask);
+        bool isHit = raycastHit.collider != null;
+
+        if (debug)
+        {
+            Color rayColor = Color.red;
+            if (isHit)
+            {
+                rayColor = Color.green;
+            }
+            Debug.DrawRay(colliderBounds.center + new Vector3(colliderBounds.extents.x - adjustOnGroundWidth, 0), Vector2.down * (colliderBounds.extents.y + extraHeight), rayColor);
+            Debug.DrawRay(colliderBounds.center - new Vector3(colliderBounds.extents.x - adjustOnGroundWidth, 0), Vector2.down * (colliderBounds.extents.y + extraHeight), rayColor);
+            Debug.DrawRay(colliderBounds.center - new Vector3(colliderBounds.extents.x - adjustOnGroundWidth, colliderBounds.extents.y + extraHeight), Vector2.right * (2 * (colliderBounds.extents.x - adjustOnGroundWidth)), rayColor);
+        }
+
+        return isHit;
+    }
+
+    /**
+     * Move player. Handles X movement, normal jumping and wall jumping.
+     */
     void Move()
     {
         transform.position += new Vector3(_horizontal, 0, 0) * Time.fixedDeltaTime * Speed;
 
-        if (_jumping)
+        if (Input.GetButton("Jump") && _leftWall)
         {
-            _jumping = false;
+            _rigidBody2D.AddForce(new Vector2(WallXJumpForce, WallYJumpForce), ForceMode2D.Impulse);
+        }
+        else if (Input.GetButton("Jump") && _rightWall)
+        {
+            _rigidBody2D.AddForce(new Vector2(-WallXJumpForce, WallYJumpForce), ForceMode2D.Impulse);
+        }
+        else if (_jumping)
+        {
             _rigidBody2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            _jumping = false;
         }
     }
 
@@ -62,6 +104,8 @@ public class PlayerMovement : MonoBehaviour
     {
         _horizontal = Input.GetAxis("Horizontal");
         _grounded = OnGround();
+        _leftWall = OnWall(-wallCheckDistance);
+        _rightWall = OnWall(wallCheckDistance);
 
         if (Input.GetButton("Jump") && _grounded)
         {
